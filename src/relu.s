2e795148129a4cf2c90b04e5ac138c7b08c5c074
Chi-Kuan Chiu <devaraja1945@gmail.com>
2024-11-17 23:31:46 +0800

    Implement relu
    - Iterate over each element in the array.
    - Replace each negative value with 0, effectively applying
    the ReLU function: x = max(0, x).

--- src/relu.s
+++ src/relu.s
@@ -24,12 +24,22 @@
 # ==============================================================================
 relu:
     li t0, 1             
-    blt a1, t0, error     
-    li t1, 0             
+    blt a1, t0, error  # beak, if the number of the array is less than 1 
+    li t1, 0             # int i = 0;
+    li t4, 0
 
 loop_start:
     # TODO: Add your own implementation
+    bge     t1, a1, exit # if i >= a1 break
+    slli    t2, t1, 2 # Multiply i by 4
+    add     t2, a0, t2 # update t2 (address of a0[i])
+    addi    t1, t1, 1 # i++
+    lw      t3, 0(t2) # t3 = a0[i] 
+    bge     t3, t4, loop_start
+    sw      t4, 0(t2) # set to zero
+    j       loop_start # jump back to start of loop
 
 error:
     li a0, 36          
     j exit          
+exit:

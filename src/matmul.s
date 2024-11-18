0c8452e4d220a10f7e7a390a81ec5d471dd5b2de
Chi-Kuan Chiu <devaraja1945@gmail.com>
2024-11-18 22:11:24 +0800

    Add epilogue and update register
    - Modify the register in the instruction from `slli t1, a5, 2`
    to `slli t1, a2, 2`. The change corrects an error caused by using `a5`
    (specific reason unknown).
    - Include an epilogue at `outerloop_end` to ensure proper cleanup and
    restoration of the state at the end of the loop.

--- src/matmul.s
+++ src/matmul.s
@@ -119,9 +119,21 @@ inner_loop_start:
 inner_loop_end:
     # TODO: Add your own implementation
     addi s0, s0, 1
-    slli t1, a5, 2
+    slli t1, a2, 2 # (why not a5 ?)
     add s3, s3, t1 # incrememtning the row on Matrix A
     j outer_loop_start
+
+outer_loop_end:
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    addi sp, sp, 28
+    jr ra   
+
 error:
     li a0, 38
     j exit
